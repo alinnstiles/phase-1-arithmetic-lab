f48fe5586b847555bfd38ffb82e272072225fcde
alex stiles <alinnstiles@gmail.com>
2022-10-19 22:37:53 -0400

    commit tree 85a5b80adbf2cf9bf272f459b5b126f9038611b2

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+//const multiply = function() {
+    
+const multiply = 62 
+const num1 = 62/num2 
+const num2 = multiply*num1
 
-// Write your code here
+//{let random = integer;
+
+//if(integer > 0) 
+
+
+const  random = integer > 0
+const integer = true 
+
+
+
+const mod = 4 
+const num3 = 4%num4 
+const num4=mod%num3 
+
+const max = 20 
+
